f511735faa17151b8b8cb09c210b2ba17671945a
Nick-v1 <nikos.voulgaridis1@gmail.com>
2022-11-07 23:42:47 +0200

    Initial Commit 3

--- Program.cs
+++ Program.cs
@@ -1,24 +1,32 @@
 ﻿using Lucene.Net.Store;
 using Lucene.Net.Util;
+using Lucene.Net.Analysis;
+using Lucene.Net.Search;
 using System;
 using System.IO;
+using Lucene.Net.Index;
 
 namespace LuceneProject
 {
     class Program
     {
         const LuceneVersion AppLuceneVersion = LuceneVersion.LUCENE_48;
-        
+        //Dataset path
+        static readonly string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\Dataset\2022-sp.csv");
+        static readonly string datasetFile = Path.GetFullPath(path);
+
         private void Run()
         {
-            //Path
-            string datasetFilePath = Path.GetFullPath(@"C:\Users\Nick\Source\Repos\Nick-v1\LuceneProject\Dataset\2022-sp.csv");
+            fileCheck(datasetFile);
+
+
             //All text from dataset
-            string text = File.ReadAllText(datasetFilePath);
+            //string text = File.ReadAllText(datasetFilePath);
+            //string[] lines = File.ReadAllLines(datasetFilePath);
 
-            
+
+
         }
-
 
 
 
@@ -31,7 +39,19 @@ namespace LuceneProject
 
 
         /// <summary>
+        /// Checks if file exists
+        /// </summary>
+        /// <param name="dtfile">The data file</param>
+        private void fileCheck(string dtfile)
+        {
+            if (File.Exists(dtfile))
+                Console.WriteLine("Dataset file exists");
+            else
+                Console.WriteLine("Dataset file not found");
+        }
+        /// <summary>
         /// Entry point
+        /// Initiates the app
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
@@ -41,3 +61,13 @@ namespace LuceneProject
         }
     }
 }
+//Βήματα
+/*1.Η ανάλυση θα βασίζεται στις μετρικές tf-idf  
+2.Η αναζήτηση θα πρέπει να αγνοεί λέξεις που είναι πολύ κοινές και δεν συνεισφέρουν  στη σημασία του κειμένου (stop-words)  
+3.Η αναζήτηση θα πρέπει να λαμβάνει υπόψη τους όρους που περιλαμβάνονται στο  κείμενο, αφού πρώτα προβεί σε αποκοπή των καταλήξεων (stemming).  
+4.Η αναζήτηση θα μπορεί να κατευθυνθεί και σε άλλα πεδία πλην αυτών που  περιέχουν το κείμενο.  
+5.Θα υπάρχει δυνατότητα να ταξινομηθούν τα αποτελέσματα, είτε με βάση κάποιο  άλλο πεδίο (πλην του κειμένου), είτε με βάση την σχετικοτητα του κειμένου τους με το  ερώτημα.  
+6.Τα αποτελέσματα θα πρέπει να μπορούν να σελιδοποιηθούν με τρόπο  παραμετροποιήσιμο (τρέχουσα σελίδα, αριθμός αποτελεσμάτων ανά σελίδα)  
+7.Η σχετικότητα εκάστου εκ των αποτελεσμάτων θα μπορεί να εμπεριέχεται στα  αποτελέσματα ως αριθμός 
+8.Τα αποτελέσματα θα πρέπει να μπορούν να παρουσιάζουν επισημασμένες τις  εμφανίσεις των λέξεων που βρέθηκαν στο κείμενο  
+9.Θα μπορούν να παρέχονται πληροφορίες για το ευρετήριο, όπως ο αριθμός  εγγράφων και οι κορυφαίοι σε εμφανίσεις όροι.*/
