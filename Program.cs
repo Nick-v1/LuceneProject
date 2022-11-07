f4f45bf04ac6132f59f68a2ccf8ed36d89d20a77
Nikolaos Voulgaridis <nikos.voulgaridis1@gmail.com>
2022-11-07 21:05:56 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using Lucene.Net.Store;
+using Lucene.Net.Util;
+using System;
+using System.IO;
+
+namespace LuceneProject
+{
+    class Program
+    {
+        const LuceneVersion AppLuceneVersion = LuceneVersion.LUCENE_48;
+        
+        private void Run()
+        {
+            var basePath = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
+            var indexPath = Path.Combine(basePath, "dataset");
+
+            using var dir = FSDirectory.Open(indexPath);
+
+            Console.WriteLine(dir);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            var self = new Program();
+            self.Run();
+        }
+    }
+}
